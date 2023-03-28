203212832c139202d49e5b03744f4d674aa295ab
shu1r0 <shu1r0.net@gmail.com>
2023-03-28 15:39:26 +0900

    fix example

--- ipnet/examples/srv6/simple_srv6_6.py
+++ ipnet/examples/srv6/simple_srv6_6.py
@@ -65,5 +65,8 @@ def setup() -> IPNetwork:
 
 if __name__ == "__main__":
     net = setup()
+    # add route
+    net.get("r1").cmd("ip -6 route add fd00:2::1/128 encap seg6 mode inline segs fd00:a::2,fd00:d::2 dev r1_r2")
+    net.get("r4").cmd("ip -6 route add fd00:1::1/128 encap seg6 mode inline segs fd00:e::1,fd00:b::1 dev r4_r3")
     CLIX(net)
     net.stop()
